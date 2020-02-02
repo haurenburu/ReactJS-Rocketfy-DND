36ca66eec22b97874299a932ba8af8f8d9d488db
haurenburu <haurenburu@protonmail.com>
2020-02-02 17:45:24 -0300

    first commit

---
+++ src/components/Header/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Container } from './style'
+
+export default function Header() {
+    return (
+        <Container>
+            <h1>Hauren!</h1>
+        </Container>
+    );
+}
